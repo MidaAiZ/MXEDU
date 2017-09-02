e00d6770799dad031a742ca65766ab76f8fbfcb0
Wu Mingda <1036570699@qq.com>
2017-09-02 23:42:09 +0800

    实现导航栏的active样式

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,17 @@
 module ApplicationHelper
+	def check_cur_ctrl(ctrl)
+	  if params[:controller] == ctrl
+		'nav-active'
+	  else
+		''
+	  end
+	end
+
+	def check_cur_act(action)
+	  if params[:action] == action
+		'active'
+	  else
+		''
+	  end
+	end
 end
