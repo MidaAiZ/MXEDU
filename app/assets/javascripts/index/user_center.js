badf36ecec10b731259d4522750589f3d350350b
Wu Mingda <1036570699@qq.com>
2017-11-02 22:03:14 +0800

    添加个人中心修改密码功能

--- app/assets/javascripts/index/user_center.js
+++ app/assets/javascripts/index/user_center.js
@@ -11,3 +11,39 @@ $(function() {
 		$("#userAvatar, .u-avatar").attr("src", res.avatar.url)
 	}
 })
+
+// 修改密码
+$(function() {
+    $("#user_pwd").on("input propertychange", function() {
+        var $this = $(this);
+        if (/^([a-zA-Z0-9]|[._,@]){8,16}$/.test($this.val())) {
+            $("#pwd-tip").text("");
+        }
+    });
+
+    $("#user_pwd").on("blur", function() {
+        var $this = $(this);
+        if (!$this.val().length) return;
+        if (!(/^([a-zA-Z0-9]|[._,@]){8,16}$/.test($this.val()))) {
+            $("#pwd-tip").text("密码不符合要求")
+        }
+    })
+
+    $("#pwd-form").on("submit", function(e) {
+        e.preventDefault();
+        var $form = $(this);
+        $.ajax({
+            url: $form.attr("action"),
+            data: $form.serialize(),
+            dataType: "JSON",
+            type: "PUT",
+            success: function(data) {
+                $("#pwdModal").modal("hide");
+            },
+            error: function(err) {
+                $("#pwd-tip").text("修改失败");
+            }
+        })
+        return false;
+    })
+})
